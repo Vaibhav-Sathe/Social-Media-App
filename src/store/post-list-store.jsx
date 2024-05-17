765f3ed5c2cb8d62ef03b24f08e7ab0fbbf546cd
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2024-05-17 12:46:58 +0530

    first commit IN SOCIAL MEDIA APP

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,81 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  }else if(action.type === "ADD_POST"){
+    newPostList = [action.payload, ...currPostList]
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider
+      value={{
+        postList,
+        addPost,
+        deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "hi friends, i am going to mumbai for my vacation",
+    reactions: 4,
+    userId: "user-9",
+    tags: ["vacations", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Pass ho gaya",
+    body: "4 saal ki masti ke baad bhi pass ho gya",
+    reactions: 14,
+    userId: "user-3",
+    tags: ["graduating", "unbeliveable"],
+  },
+];
+
+export default PostListProvider;
